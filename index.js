20b34cf749888300daf5112deb3be5197d1f6f6c
robiulXhasan <robi16275@gmail.com>
2023-05-18 10:45:13 +0600

    server initialized and middleware installed

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ToyLand server Running");
+});
+
+app.listen(port, () => console.log(`ToyLang Running on port : ${port}`));
